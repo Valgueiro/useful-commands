159901cd96d98fc1084ff1c02059bbd738488d2f
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2024-06-17 17:34:08 -0300

    ta uma merda

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,26 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = {"go", "lua", "javascript", "python", "hcl", "yaml", "json", "gitignore", "bash", "markdown"},
-    highlight = { enabled = true },
-    auto_install = true,
-    indent = { enabled = true },
-  },
-  config = function(_, opts)
-    require('nvim-treesitter.install').prefer_git = true
-    ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup(opts)
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	opts = {
+		ensure_installed = {
+			"go",
+			"lua",
+			"javascript",
+			"python",
+			"hcl",
+			"yaml",
+			"json",
+			"gitignore",
+			"bash",
+			"markdown",
+		},
+		highlight = { enabled = true },
+		auto_install = true,
+		indent = { enabled = true },
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.install").prefer_git = true
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
