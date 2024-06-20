5830a469a3240676d426d338acc7c846d3d3d485
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2024-06-20 11:25:02 -0300

    Ädd neotree on startup

--- dotfiles/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/nvim/lua/plugins/neo-tree.lua
@@ -35,5 +35,16 @@ return {
 			},
 		})
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
+
+		vim.api.nvim_create_augroup("neotree", {})
+		vim.api.nvim_create_autocmd("UiEnter", {
+			desc = "Open Neotree automatically",
+			group = "neotree",
+			callback = function()
+				if vim.fn.argc() == 0 then
+					vim.cmd("Neotree toggle")
+				end
+			end,
+		})
 	end,
 }
