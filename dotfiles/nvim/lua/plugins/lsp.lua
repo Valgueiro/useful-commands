b7c4349852e4460b95c62fd90accef8d70956637
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2024-06-20 11:20:25 -0300

    Add nvim tmux

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -5,7 +5,12 @@ return {
 			-- Automatically install LSPs and related tools to stdpath for Neovim
 			{ "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
 			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			{
+				"WhoIsSethDaniel/mason-tool-installer.nvim",
+				opts = {
+					auto_install = true,
+				},
+			},
 
 			-- Useful status updates for LSP.
 			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
---
+++ dotfiles/nvim/lua/plugins/nvim-tmux.lua
@@ -0,0 +1,10 @@
+return {
+	"alexghergh/nvim-tmux-navigation",
+	config = function()
+		require("nvim-tmux-navigation").setup({})
+		vim.keymap.set("n", "<C-Left>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
+		vim.keymap.set("n", "<C-Down>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
+		vim.keymap.set("n", "<C-Up>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
+		vim.keymap.set("n", "<C-Right>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
+	end,
+}
