9947cf571ae36816068df0129ca27d269d1a8513
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2024-07-04 11:46:51 -0300

    add copilot auto suggestion to another keybing

--- dotfiles/nvim/lua/plugins/copilot.lua
+++ dotfiles/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,11 @@
 return {
 	"github/copilot.vim",
+	config = function()
+		vim.keymap.set("i", "<C-A>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+			desc = "[A]ccept completion",
+		})
+		vim.g.copilot_no_tab_map = true
+	end,
 }
