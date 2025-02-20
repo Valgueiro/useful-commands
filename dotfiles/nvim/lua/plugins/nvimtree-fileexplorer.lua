2d3d1f2297958b4a75d2c39c2d4870f46674761c
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2025-02-20 02:24:20 -0300

    Change split keymap and rename to make it more accurate with latest changes

--- dotfiles/nvim/lua/plugins/nvimtree-fileexplorer.lua
+++ dotfiles/nvim/lua/plugins/nvimtree-fileexplorer.lua
@@ -1,9 +1,25 @@
+local function my_on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	vim.keymap.set("n", "s", api.node.open.vertical, opts("Open: Vertical Split"))
+	vim.keymap.set("n", "h", api.node.open.horizontal, opts("Open: Horizontal Split"))
+end
+
 return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 	},
 	config = {
+		on_attach = my_on_attach,
 		sort = {
 			sorter = "case_sensitive",
 		},
