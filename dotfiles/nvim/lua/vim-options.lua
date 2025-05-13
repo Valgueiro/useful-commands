3807872b07bd7f6fd2de3775470aa6b264c2c766
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2025-05-13 11:22:17 -0300

    Dont show nvimtree when it is not needed and quit when it is the only missing tab

--- dotfiles/nvim/lua/vim-options.lua
+++ dotfiles/nvim/lua/vim-options.lua
@@ -111,6 +111,22 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+-- Nvimtree file explorer auto close when it is the last one
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("NvimTreeClose", { clear = true }),
+	pattern = "NvimTree_*",
+	callback = function()
+		local layout = vim.api.nvim_call_function("winlayout", {})
+		if
+			layout[1] == "leaf"
+			and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree"
+			and layout[3] == nil
+		then
+			vim.cmd("confirm quit")
+		end
+	end,
+})
+
 -- vim.keymap.set("n", "<Leader>b", ":buffers<CR>:buffer<Space>", { desc = "List buffers" })
 vim.keymap.set({ "v", "n", "x" }, "<leader>p", [["_dP]])
 vim.keymap.set("n", "<C-W>z", "<C-W>o", { desc = "Zoom" })
